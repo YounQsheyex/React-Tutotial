cb9e4d6eddb17195341a5001a0c3056391d11023
YounQsheyex <younqsheyex@gmail.com>
2025-05-08 15:44:25 +0100

    react typed

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,13 +1,22 @@
 import React from "react";
 import { useContext } from "react";
 import { AppContext } from "../context";
+import { ReactTyped } from "react-typed";
 const Hero = () => {
   const { value1 } = useContext(AppContext);
   return (
     <div className=" bg-black text-white h-screen max-h-[600px] ">
       <div className="container h-full flex flex-col lg:flex-row items-center justify-between gap-6">
         <div className="max-w-[600px]">
-          <h1 className="text-2xl md:text-4xl">Buy Luxry Cars</h1>
+          <h1 className="text-2xl md:text-4xl">
+            Buy a {""}
+            <ReactTyped
+              typeSpeed={80}
+              backSpeed={90}
+              strings={["Car", "Jet", "Bus"]}
+              loop
+            />
+          </h1>
           <h1 className="text-2xl md:text-4xl mt-3">{value1}</h1>
           <p className="my-4">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat at
